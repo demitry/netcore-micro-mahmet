69477bcfb7859325a2d88311d9cd2f036d17c905
demitry <dpoluektov@gmail.com>
2024-03-22 18:15:16 +0200

    Add Docker Compose Orchestration Support, Connection String for the Db with Marten

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,35 +1,30 @@
-﻿using BuildingBlocks.CQRS;
-using Catalog.API.Models;
+﻿namespace Catalog.API.Products.CreateProduct;
 
-namespace Catalog.API.Products.CreateProduct
+public record CreateProductCommand(string Name, List<string> Category, string Summary, string Description, string ImageFile, decimal Price)
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(string Id);
+
+internal class CreateProductCommandHandler (IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-
-    public record CreateProductCommand(string Name, List<string> Category, string Summary, string Description, string ImageFile, decimal Price)
-        : ICommand<CreateProductResult>;
-    public record CreateProductResult(Guid Id);
-
-    internal class CreateProductCommandHandler 
-        : ICommandHandler<CreateProductCommand, CreateProductResult>
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
-        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        // Create a product entity from incoming command
+        var product = new Product
         {
-            // Create a product entity from incoming command
-            var product = new Product
-            {
-                Name = command.Name,
-                Category = command.Category,
-                Summary = command.Summary,
-                Description = command.Description,
-                ImageFile = command.ImageFile,
-                Price = command.Price
-            };
+            Name = command.Name,
+            Category = command.Category,
+            Summary = command.Summary,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
 
-            // TODO:
-            // Save to database
+        // Save to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
-            // Return the result
-            return new CreateProductResult(Guid.NewGuid());
-        }
+        // Return the result
+        return new CreateProductResult(product.Id);
     }
 }
-    
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -3,11 +3,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add Services to DI container
 builder.Services.AddCarter();
 
-
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
 });
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
 
 var app = builder.Build();
 
